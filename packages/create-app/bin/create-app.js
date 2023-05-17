cf5833c3d9c49542cd598ce6731c28b891fa5d6c
zhangyong.86 <zhangyong.86@bytedance.com>
2023-05-17 15:50:00 +0800

    feat: 初始化脚手架

--- packages/create-app/bin/create-app.js
+++ packages/create-app/bin/create-app.js
@@ -1,41 +1,38 @@
 #!/usr/bin/env node
 
-let program = require('commander');
+const program = require('commander');
+const inquirer = require('inquirer');
+const Mustache = require('mustache');
+const chalk = require('chalk'); // 5版本使用es模块，4及4以下是commonjs
+const figlet = require('figlet'); // 终端输出logo
+
 const fs = require('fs');
 const path = require('path');
 const CWD = process.cwd();
 const pkg = require('../package.json');
+const create = require('../lib/create.js');
 
 program.version(pkg.version);
 
-program.command('build').description('打包cli成功')
+program.on('--help', () => {
+  console.log(`\r\n${figlet.textSync('create-app', {
+    font: 'Ghost',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 500,
+    whitespaceBreak: true
+  })}`)
+  // 新增信息说明
+  console.log(`\r\n${chalk.cyan(`create-app <command> --help`)} for detailed usage of givern command \r\n`)
+})
 
+// create 命令
 program
-  .command('create-app <name> [options]')
-  .description('创建项目名称')
-  .action(function(name, options){
-    console.log(name);
-    console.log(options);
-  });
-
-program
-  .command('config')
-  .description('读取配置文件')
-  .action((appName, options) => {
-    let config = {
-      path: 'svg',
-    }
-
-    const configPath = CWD + '/src/config.js';
-    console.log(CWD);
-    // 如果使用了配置文件，则以配置文件为准
-    if (fs.existsSync(configPath)) {
-      const userConfig = require(path.resolve(CWD, 'src/config.js'))
-      config = { ...config, ...userConfig }
-      console.log(`存在配置文件config.js，获取到的名字为`, config.name)
-    } else {
-      console.log(`不存在配置文件`)
-    }
+  .command('create <name> [options]')
+  .description('创建新项目')
+  .option('-f --force', '强制覆盖已存在文件')
+  .action(async (name, option) => {
+    create(name, option);
   })
 
 program.parse(process.argv);
